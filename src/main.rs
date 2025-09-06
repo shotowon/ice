7717bf7b8f9d60c4c8ef5ea4b8f4427716966d83
shotowon <sanzhar.maratov@proton.me>
2025-09-07 01:10:52 +0500

    use lexer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use colored::*;
+
+use std::{env, fs::File, io::Read, process};
+
+use ice::lexer::Lexer;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("{}", "failed to compile 'ice' program".red().bold());
+        eprintln!("{}", "usage:".bright_blue());
+        eprintln!("\t{} {}", args[0].green(), "<your-file.ic>".blue().bold());
+        process::exit(1);
+    }
+
+    let mut file = File::open(args[1].clone()).expect(
+        format!(
+            "{}: {}",
+            "failed to open file".red().bold(),
+            args[1].green(),
+        )
+        .as_str(),
+    );
+
+    let mut src = String::new();
+    file.read_to_string(&mut src)
+        .expect(format!("{}", "failed to read file contents".red(),).as_str());
+
+    let mut lexer = Lexer::new(src.into());
+    match lexer.lex() {
+        Ok(tokens) => {
+            for token in tokens {
+                println!("token: {:?}", token)
+            }
+        }
+        Err(err) => {
+            eprintln!("{}: {}", "lexical error".red().bold(), err.bright_red());
+            process::exit(1);
+        }
+    }
 }
