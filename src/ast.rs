2d8e2f200615245446f48f5c229046f0f3eab61a
shotowon <sanzhar.maratov@proton.me>
2025-09-14 01:42:30 +0500

    AST: debug functions for AST nodes

--- src/ast.rs
+++ src/ast.rs
@@ -1,4 +1,5 @@
 use crate::tokens::{Token, TokenKind};
+use std::fmt;
 
 pub enum Statement {
     Return {
@@ -30,4 +31,44 @@ pub enum Expression {
     Int {
         value: Token,
     },
+}
+
+impl fmt::Display for Statement {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Statement::ExpressionStatement { expression } => {
+                write!(f, "{};", expression)
+            },
+            Statement::Return { value } => {
+                if let Some(val) = value {
+                    write!(f, "return {}", val)
+                } else {
+                    write!(f, "return")
+                }
+            },
+        }
+    }
+}
+
+impl fmt::Display for Expression {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Expression::Binary { lhs, op, rhs } => {
+                write!(f, "({} {} {})", lhs, op, rhs)
+            }
+            Expression::Unary { op, expr } => {
+                write!(f, "({}{})", op, expr)
+            }
+            Expression::FunctionCall { name, args } => {
+                let args_str: Vec<String> = args.iter().map(|a| a.to_string()).collect();
+                write!(f, "fcall: {}({})", name.literal, args_str.join(", "))
+            }
+            Expression::Id { name } => {
+                write!(f, "{}", name.literal)
+            }
+            Expression::Int { value } => {
+                write!(f, "{}", value.literal)
+            }
+        }
+    }
 }
